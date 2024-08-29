9576ec32d949b0fff5cbf3bccdcfd12ca321894f
warren-west <teacher.warren.english@gmail.com>
2024-08-29 13:11:47 +0200

    Added extra recursion example

--- Module 2/recursion.js
+++ Module 2/recursion.js
@@ -1,19 +1,34 @@
-
+// Regular function
 function addTwoNumbers(num1, num2) {
     return num1 + num2
 }
 
+// Recursive function (it invokes itself)
 function recursionExample(value) {
 
     let newValue = value - 1
 
     console.log("Before value:", value)
-    
+
     if (newValue > 0) {
-        recursionExample(newValue)
+        recursionExample(newValue) // <--- Here is the recursion! Opens up a new instance of the function.
     }
 
     console.log("After value:", value)
 }
 
-// recursionExample(3)
+/* EXTRA EXAMPLE */
+
+function reverseString(str) {
+    console.log(str)
+    if (str === '') {
+        return ''
+    } else {
+        let restOfWord = str.substring(1) // "hello".substring(1) = "ello"
+        return reverseString(restOfWord) + str[0]
+    }
+}
+
+const inputStr = "hello"
+const reversedStr = reverseString(inputStr)
+console.log(reversedStr) // Output: "olleh"
